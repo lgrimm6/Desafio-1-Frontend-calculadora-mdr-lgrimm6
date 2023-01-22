333f482b211077b43157d947795b54b42624695f
lgrimm6 <robertofernandesfreitas@gmail.com>
2023-01-22 02:31:02 -0300

    Feat: componentization of the Input

--- src/Components/Input/index.tsx
+++ src/Components/Input/index.tsx
@@ -1,19 +1,30 @@
 import { InputHTMLAttributes } from "react";
-import InputLabel from "./style";
+import { FieldValues, UseFormRegister } from "react-hook-form";
+import Label from "Components/Label";
+import InputStyle from "./style";
 
 interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  register: UseFormRegister<FieldValues>;
   placeholder?: string;
   label: string;
   span?: string;
 }
 
-const Input = ({ placeholder, label, span, ...rest }: IInputProps) => {
+const Input = ({
+  placeholder,
+  label,
+  span,
+  register,
+  name,
+  ...rest
+}: IInputProps) => {
   return (
-    <InputLabel>
+    <Label>
       {label}
-      <input placeholder={placeholder} {...rest} />
+      <InputStyle placeholder={placeholder} {...register(name)} {...rest} />
       {span && <span>{span}</span>}
-    </InputLabel>
+    </Label>
   );
 };
 
--- src/Components/Input/style.ts
+++ src/Components/Input/style.ts
@@ -1,25 +1,14 @@
 import styled from "styled-components";
 
-const InputLabel = styled.label`
-  display: flex;
-  flex-direction: column;
-  font-size: 14px;
-  color: #656565;
-  gap: 6px;
-  input {
-    width: 251px;
-    height: 37px;
-    border: 1px solid #dde6e9;
-    border-radius: 4px;
-
-    &:hover {
-      border-color: #5d9cec;
-    }
-  }
-  span {
-    font-size: 11px;
-    color: #cecece;
+const InputStyle = styled.input`
+  width: 251px;
+  height: 37px;
+  border: 1px solid #dde6e9;
+  border-radius: 4px;
+  padding: 5px;
+  &:hover {
+    border-color: #5d9cec;
   }
 `;
 
-export default InputLabel;
+export default InputStyle;
