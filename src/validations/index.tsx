bc0de326f0dd76241d6210f22a96b95bc9319429
lgrimm6 <robertofernandesfreitas@gmail.com>
2023-01-22 21:24:41 -0300

    Fix: yup schema

--- src/validations/index.tsx
+++ src/validations/index.tsx
@@ -5,11 +5,18 @@ const schemaForm = yup.object().shape({
     .number()
     .min(1000)
     .max(100000000)
+    .typeError("No minimo 1000")
     .required("Informe o valor em cantavos"),
   installments: yup.number(),
-  mdr: yup.number().required("informe a % de MDR"),
+  mdr: yup
+    .number()
+    .min(1)
+    .max(99)
+    .typeError("informe a % de MDR de 0 a 99")
+    .required("informe a % de MDR"),
   days: yup
     .string()
+    .max(28)
     .matches(/^\d+(,\d+)*$/, {
       message: "valor invalido",
       excludeEmptyString: true,
